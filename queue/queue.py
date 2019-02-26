0b6d988842adb428cb4b6dd7b4546304a204bf3d
Rahul Desai <rd825@georgetown.edu>
2019-02-26 12:39:20 -0500

    Added LinkedList pseudocode comments to queue.py

--- queue/queue.py
+++ queue/queue.py
@@ -3,16 +3,17 @@ class Queue:
         self.size = 0
         # what data structure should we
         # use to store queue elements?
-        self.storage = []
+        self.storage = []  # could be LinkedList or 2 Stacks as alternative
 
     def enqueue(self, item):
-        return self.storage.append(item)
+        return self.storage.append(item)  # add_to_tail in LL, increment size
 
     def dequeue(self):
-        if len(self.storage) == 0:
+        if len(self.storage) == 0:  # if size = 0, return size
             return None
         else:
+            # remove_from_head in LL, decrement size
             return self.storage.pop(0)
 
     def len(self):
-        return len(self.storage)
+        return len(self.storage)  # return size
